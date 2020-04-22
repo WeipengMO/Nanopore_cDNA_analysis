cb970b9f0f8d408fd532de887dbf768228cef367
windz <windzlife@gmail.com>
2020-04-22 09:40:54 +0800

    find raw splice sites

--- script/find_splice_sites.py
+++ script/find_splice_sites.py
@@ -3,124 +3,119 @@
 '''
 @Author       : windz
 @Date         : 2020-04-17 17:26:26
-@LastEditTime : 2020-04-18 20:27:32
+@LastEditTime : 2020-04-20 22:32:26
 @Description  : Find splice sites in a bed file created with the -split option
 '''
 
+from collections import namedtuple
+import numpy as np
 import click
 
 
-def get_spliced_transcript(exons):
-    start = None
-    lastexon = None
-    spliceSites = []
-    for exon in exons:
-        #the first part of each read should be the same chromosome name
-        chro = exon[0]
-        #the end position of the previous exon is the start of a splice site
-        if lastexon is not None:
-            spliceSites.append(lastexon[2])
-        #the start position of the first exon should be the start of the transcript
-        if start is None:
-            start = exon[1]
-        #the start position of the other exons are the end of splice sites
-        else:
-            spliceSites.append(exon[1])
-        lastexon = exon
-    #the remaining end position at the end of the loop is the end of the transcript
-    end = lastexon[2]
-    strand = lastexon[5]
-    return [chro, start, end, strand, spliceSites]
-    
+def get_spliced_transcript(last_line, spliced_sites_list):
+    chro = last_line.chro
+    start = spliced_sites_list[0]
+    end = spliced_sites_list[-1]
+    read_id = last_line.read_id
+    strand = last_line.strand
+    spliced_sites = ','.join(spliced_sites_list[1: -1])
+
+    return chro, start, end, strand, spliced_sites, read_id
+
 
 def get_isoform(transcripts):
-    #if len(transcripts) <= 0:
-    #    return["",0,0,0,[]]
-    start = 0
-    end = 0
-    for transcript in transcripts:
-        #the first part of each transcript should be the same chromosome name
-        chro = transcript[0]
-        #the final start and end positions will be the mean of all start and end positions, respectively
+    # 计算这一isoform cluster的 start 和 end 的加权平均值
+    start, end = 0, 0
+    read_id = []
+    for count, transcript in enumerate(transcripts, 1):
         start += int(transcript[1])
         end += int(transcript[2])
-    #the last part of each transcript should be the same splice sites
-    spliceSites = transcript[4]
+        read_id.append(transcript[5])
+
+    chro = transcript[0]
+    start = round(start/count)
+    end = round(end/count)
     strand = transcript[3]
-    len_transcript = len(transcripts)
-    return [chro, round(start/len_transcript), round(end/len_transcript), len_transcript-1, strand, spliceSites]
+    spliced_sites = transcript[4]
+    read_id = ','.join(read_id)
+
+    return '\t'.join([chro, str(start), str(end), strand, str(count), spliced_sites, read_id])
 
 
 @click.command()
 @click.option('--infile', required=True)
 @click.option('--outfile', required=True)
-def find_splice_sites(infile, outfile):
+@click.option('--max_difference', required=False, default = 10)
+@click.option('--min_transcript_count', required=False, default = 3)
+def find_splice_sites(infile, outfile, max_difference, min_transcript_count):
     # read the file and compare each line with its neighbours
-    # input: bed file
-    # format: 0: chr  1: start  2: end  3: read_id  4: score  5: strand
-    with open(infile, "r") as bedFile:
-        firstline = True
-        # bed文件上一行数据
-        lastline = None
-        exons = []
-        splicedTranscirpts = []
-        for line in bedFile:
-            #Seach for duplicated instances of reads in the split file (split sites) by comparing their names
-            currentline=line.rstrip().split("\t")
-            # 如果为同一read_id, 则两行之间为splice区间
-            if lastline is not None and lastline[3] == currentline[3]:
-                if firstline:
-                    exons.append(lastline)
-                    firstline=False
-                # 该read_id的exon位置
-                exons.append(currentline)
-            else:
-                #If we found a duplicate, then we add it to the list along with its splice sites
-                if not firstline:
-                    splicedTranscirpts.append(get_spliced_transcript(exons))
-                #When a new read name is found, reinitialize the variables
-                exons=[]
-                firstline=True
-            lastline=currentline
+    # infile: bedfile
+    # format: 0: chro  1: start  2: end  3: read_id  4: score  5: strand
+    Bedformat = namedtuple('Bedformat', 'chro start end read_id score strand')
 
-        #If the last lines were a duplicate, then we add it to the list along with its splice sites
+    last_line = None
+    spliced_sites_list = []
+    spliced_transcirpts = []
+    intronless_transcirpts = []
+    # Seach for duplicated instances of reads in the split file (split sites) by comparing their names
+    with open(infile, 'r') as bedFile:
+        for line in bedFile:
+            chro, start, end, read_id, score, strand = line.rstrip().split('\t')
+            current_line = Bedformat(chro, start, end, read_id, score, strand)
+            # 下一个read_id初始化
+            if last_line is not None and current_line.read_id != last_line.read_id:
+                if len(spliced_sites_list) > 2:
+                    spliced_transcirpts.append(get_spliced_transcript(last_line, spliced_sites_list))
+                else:
+                    intronless_transcirpts.append(get_spliced_transcript(last_line, spliced_sites_list))
+                spliced_sites_list = []
+            spliced_sites_list.append(current_line.start)
+            spliced_sites_list.append(current_line.end)
+            last_line = current_line
+
         # 处理最后一条read
-        if not firstline:
-            splicedTranscirpts.append(get_spliced_transcript(exons))
+        spliced_transcirpts.append(get_spliced_transcript(last_line, spliced_sites_list))
 
     #sort our duplicate list by key, then by chromosome name for later manipulation
-    splicedTranscirpts.sort(key=lambda transcript: transcript[3])
-    splicedTranscirpts.sort(key=lambda transcript: transcript[0])
+    spliced_transcirpts.sort(key=lambda transcript: transcript[3]) # sort by strand
+    spliced_transcirpts.sort(key=lambda transcript: transcript[4]) # sort by splice sites
+    spliced_transcirpts.sort(key=lambda transcript: transcript[0]) # sort by chr
 
-    #Keep only the transcripts with at least 3
-    isoforms = []
-    currentIsoform = []
-    firstline = True
-    lastSpliceSites = None
-    # splicedTranscripts is a list contains [chro, start, end, spliceSites]
-    for transcript in splicedTranscirpts:
-        spliceSites = " ".join(transcript[4])
-        # 完全一样？无考虑测序错误情况
-        # TODO(windz) 可优化
-        if spliceSites == lastSpliceSites:
-            currentIsoform.append(transcript)
-        else:
-            if firstline:
-                firstline=False
-            else:
-                #Add the isoform to the list if there are at least 3 instances of it
-                if len(currentIsoform) > 2:
-                    isoforms.append(get_isoform(currentIsoform))
-
-            currentIsoform=[transcript]
-        # 上一个splice sites的位置(字符串)
-        lastSpliceSites = spliceSites
-
-
-    with open(outfile, "w") as outFile:
-        for transcript in isoforms:
-            # chro, start, end, length, spliceSites
-            outFile.write(f'{transcript[0]}\t{transcript[1]}\t{transcript[2]}\t{transcript[3]}\t{transcript[4]}\t{",".join(transcript[5])}\n')
+    with open(outfile, 'w') as out:
+        # for intron-contained transcripts
+        last_transcript = None
+        current_isoform = []
+        for transcript in spliced_transcirpts:
+            if last_transcript is not None and transcript[-2] != last_transcript[-2]:
+                # 有3条reads支持的isoform (min_transcript_count)
+                if len(current_isoform) >= min_transcript_count:
+                    out.write(get_isoform(current_isoform)+'\n')
+                # 重新初始化
+                current_isoform = [transcript]
+            elif last_transcript is not None and transcript[-2] == last_transcript[-2]:
+                current_isoform.append(transcript)
+            elif last_transcript is None:
+                current_isoform.append(transcript)
+            last_transcript = transcript
+        if len(current_isoform) >= min_transcript_count:
+            out.write(get_isoform(current_isoform)+'\n')
+        
+        # for intronless transcripts
+        last_transcript = None
+        current_isoform = []
+        for transcript in intronless_transcirpts:
+            current_position = np.array((int(transcript[1]), int(transcript[2])))
+            if (last_transcript is not None and 
+                    (last_transcript[3] != transcript[3] 
+                        or not(abs(last_position - current_position) < max_difference).all())):
+                if len(current_isoform) >= min_transcript_count:
+                    out.write(get_isoform(current_isoform)+'\n')
+                current_isoform = []
+            current_isoform.append(transcript)
+            last_position = current_position
+            last_transcript = transcript
+        if len(current_isoform) >= min_transcript_count:
+            out.write(get_isoform(current_isoform)+'\n')
 
 
 if __name__ == '__main__':
